8da2e0fc0b9e4cf3d797b5e2c2af2b7ad76b5d17
Caca404 <christia10k@hotmail.com>
2023-01-16 10:21:16 -0300

    :sparkles: feat: add back-end with adonis and initiate project angular

--- moments/src/app/app.component.spec.ts
+++ moments/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'first-project-angular'`, () => {
+  it(`should have as title 'moments'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('first-project-angular');
+    expect(app.title).toEqual('moments');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('first-project-angular app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('moments app is running!');
   });
 });
