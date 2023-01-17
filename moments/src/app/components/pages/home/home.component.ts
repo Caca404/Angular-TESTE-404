c6f37d79af3d3740f13fd83d2cb1cd7fa66c17bf
Caca404 <christia10k@hotmail.com>
2023-01-17 10:15:32 -0300

    :racehorse: feat: create homepage to pull all moments from database and filter by search

--- moments/src/app/components/pages/home/home.component.ts
+++ moments/src/app/components/pages/home/home.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MomentService } from 'src/app/services/moment.service';
+import { Moment } from 'src/app/models/Moment';
+import { environment } from 'src/environments/environment';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  allMoments: Moment[] = [];
+  moments : Moment[] = [];
+  baseApiUrl = environment.baseApiUrl;
 
+  faSearch = faSearch;
+  searchTerm: string = "";
+
+  ngOnInit(): void{
+    this.momentService.getMoments().subscribe((items) => {
+      const data = items.data;
+
+      data.map((item) => {
+        item.created_at = new Date(item.created_at!).toLocaleDateString('pt-BR');
+      });
+
+      this.allMoments = data;
+      this.moments = data;
+    });
+  }
+  
+  constructor(private momentService: MomentService) {}
+
+  search(event: Event): void{
+    const target = event.target as HTMLInputElement;
+    const value = target.value;
+
+    this.moments = this.allMoments.filter((moment) => {
+      return moment.title.toLowerCase().includes(value);
+    });
+  }
+  
 }
